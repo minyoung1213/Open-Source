34e387b6e27cbd1f0170b8e8bb5419a318a248a4
linear <lineark2m@gmail.com>
2025-06-21 00:20:36 +0900

    Update warning_output.py

--- detector/warning_output.py
+++ detector/warning_output.py
@@ -7,10 +7,6 @@ class WarningOutput:
         self.engine = pyttsx3.init()
         self.engine.setProperty('rate', 150)
 
-    def show_on_frame(self, frame):
-        cv2.putText(frame, "Illegal Dumping Detected!", (100, 300),
-                    cv2.FONT_HERSHEY_SIMPLEX, 4.0, (0, 0, 255), 5)
-
     def speak_once(self):
         if not self.triggered:
             self.engine.say("쓰레기 버리지 마세요.")
